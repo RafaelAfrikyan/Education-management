cfb5b0ee0c77679d91e767cafe20ce4b960e0ac7
AvetBadalyan <avetbadalyan@gmail.com>
2022-05-18 12:55:07 +0400

    feat(pagestructure): create page structuring

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./Components/Layout.jsx";
-import Login from "./Components/Login.jsx";
+import Lecturers from "./Pages/Lecturers/Lectures.jsx";
 import Home from "./Components/Home.jsx";
-import Lectures from "./Components/Lectures.jsx";
-
-import Course from "./Components/Course.jsx";
 import CreateClasslist from "./Components/CreateClasslist.jsx";
 // import Room from "./Components/Hall.jsx";
 import RoomsTimetable from "./Components/RoomsTimetable.js";
-import Lecturer from "./Components/Lecturer.js";
+import Lecturer from "./Pages/Lecturer/Lecturer.js";
 import Faculties from "./Pages/Faculties/Faculties.jsx";
-import FacultiesTimetable from "./Pages/Faculties/FacultiesTimetable.jsx";
+import Login from "./Pages/Login/Login.jsx";
+import FacultiesTimetable from "./Components/FacultiesTimetable.jsx";
+
+
 
 function App() {
   return (
@@ -21,10 +21,10 @@ function App() {
 
         <Route path="/home" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="lectures" element={<Lectures />} />
+          <Route path="lectures" element={<Lecturers />} />
           <Route path="home/:roomId" element={<RoomsTimetable />} />
           <Route path="lectures/:lecturerName" element={<Lecturer />} />
-          <Route path="course/:faculty" element={<FacultiesTimetable />} />
+          <Route path="course/:faculty" element={<FacultiesTimetable/>} />
           <Route path="course" element={<Faculties />} />
           <Route path="create" element={<CreateClasslist />} />
         </Route>
