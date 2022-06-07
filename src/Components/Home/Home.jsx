9fe1c5533b38527098035e7c1ca3c21619d317b8
AvetBadalyan <avetbadalyan@gmail.com>
2022-06-07 15:17:18 +0400

    style(home and login): changes in styles

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <div className="home-page">
       <h1 className="welcome=text"> WELCOME TO CUSTOM STUDY MANAGEMENT WEBSITE</h1>
-      <button className="login-button"> Login / Sign up </button>
       <main className="home-main"> </main>
     </div>
   )
