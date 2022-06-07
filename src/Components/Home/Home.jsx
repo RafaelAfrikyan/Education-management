36cf949af00be7336866b6daced54c528a641bac
AvetBadalyan <avetbadalyan@gmail.com>
2022-06-07 14:29:54 +0400

    feat(home page): add some styles and content  to home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "./Home.css"
 
 export default function Home() {
   return (
-    <div>This is Home page</div>
+    <div className="home-page">
+      <h1 className="welcome=text"> WELCOME TO CUSTOM STUDY MANAGEMENT WEBSITE</h1>
+      <button className="login-button"> Login / Sign up </button>
+      <main className="home-main"> </main>
+    </div>
   )
 }
