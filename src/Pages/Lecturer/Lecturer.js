17e0dac9f591cc6dd661e2c8ea0ab7600a912482
AvetBadalyan <avetbadalyan@gmail.com>
2022-06-08 17:10:25 +0400

    feat(lecturer info): add a card with lecturer info in single lecturer page

--- src/Pages/Lecturer/Lecturer.js
+++ src/Pages/Lecturer/Lecturer.js
@@ -4,6 +4,7 @@ import DrawTable from "../../Components/DrawTable/DrawTable.js";
 import LecturerInfo from "../../Components/LecturerInfo/LecturerInfo.js";
 import LecturersTimetable from "../../Components/LecturersTimetable/LecturersTimetable.js";
 import { lecturers } from "../../state/state.js";
+import "./Lecturer.css";
 
 function Lecturer() {
   const { lecturerName } = useParams();
@@ -12,8 +13,15 @@ function Lecturer() {
   );
   const { name, surname, speciality } = filtered[0];
   return (
-    <div>
-      <LecturerInfo />
+    <div className="singleLecturer">
+      <div className="lecturer-info">
+        <img src="https://images1.content-hcs.com/commimg/myhotcourses/blog/post/myhc_78689.jpg" />
+        <div className="lecturer-name">
+          <h3> Name: {name} </h3>
+          <h3> Surname: {surname} </h3>
+          <h3> Speciality: {speciality}</h3>
+        </div>
+      </div>
       <DrawTable param={filtered[0].timeTable} />
     </div>
   );
