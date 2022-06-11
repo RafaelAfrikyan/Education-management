e99cafffc9efe919099e5bdad11ee4be8ae757ff
AvetBadalyan <avetbadalyan@gmail.com>
2022-06-12 00:43:37 +0400

    style(home page styling): styling home page and fit it with footer

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { ACTION_TYPES } from "../../state/state";
 import { useState } from "react";
+import "./Home.css";
 
 export default function Home() {
   const dispatch = useDispatch();
@@ -47,47 +48,49 @@ export default function Home() {
   console.log(lecturers);
 
   return (
-    <div>
-      <div>
-        <div>Add Lecturer</div>
-        <div>
-          <input
-            onChange={setLecturerName}
-            type="text"
-            placeholder="lecturer name"
-            value={isLecturerName}
-          />
-          <input
-            onChange={setLecturerSurname}
-            type="text"
-            placeholder="lecturer surname"
-            value={isLecturerSurname}
-          />
-          <input
-            value={isLecturerSpeciality}
-            onChange={(e) => setIsLecturerSpeciality(e.target.value)}
-            type="text"
-            placeholder="speciality"
-          />
-          <button onClick={addLecturer}>Add</button>
+    <div className="home-page">
+      <div className="add-inputs-modal">
+        <div className="add-lecturer">
+          <h1>Add Lecturer</h1>
+          <div className="add-lecturer-input">
+            <input
+              onChange={setLecturerName}
+              type="text"
+              placeholder="lecturer name"
+              value={isLecturerName}
+            />
+            <input
+              onChange={setLecturerSurname}
+              type="text"
+              placeholder="lecturer surname"
+              value={isLecturerSurname}
+            />
+            <input
+              value={isLecturerSpeciality}
+              onChange={(e) => setIsLecturerSpeciality(e.target.value)}
+              type="text"
+              placeholder="speciality"
+            />
+            <button onClick={addLecturer}>Add</button>
+          </div>
         </div>
-      </div>
-      <div>
-        <div>Add Room</div>
-        <div>
-          <input
-            onChange={(e) => setisRoomSize(e.target.value)}
-            value={isRoomSize}
-            type="number"
-            placeholder="roomsize"
-          />
-          <input
-            onChange={(e) => setisRoomNumber(e.target.value)}
-            value={isRoomNumber}
-            type="number"
-            placeholder="number"
-          />
-          <button onClick={addRoom}>Add</button>
+        <div className="add-room">
+          <h1>Add Room</h1>
+          <div className="add-room-input">
+            <input
+              onChange={(e) => setisRoomSize(e.target.value)}
+              value={isRoomSize}
+              type="number"
+              placeholder="roomsize"
+            />
+            <input
+              onChange={(e) => setisRoomNumber(e.target.value)}
+              value={isRoomNumber}
+              type="number"
+              placeholder="number"
+            />
+            <button onClick={addRoom}>Add</button>
+          </div>
         </div>
       </div>
     </div>
