4d261202f4c93a3be8f9a9337db965491b11384e
AvetBadalyan <avetbadalyan@gmail.com>
2022-06-14 16:12:01 +0400

    feat(room): add the room number in every room  page dynamically

--- src/Components/RoomsTimetable/RoomsTimetable.js
+++ src/Components/RoomsTimetable/RoomsTimetable.js
@@ -14,14 +14,15 @@ function RoomsTimetable() {
   const { roomId } = useParams();
   console.log(roomId)
   return (
-      <div className="wrapperRoom">
-        {Object.keys(rooms).map((size, i) =>
-          Object.keys(rooms[size]).map(
-            (room, i) =>
-              room == roomId && <DrawTable key={i} param={rooms[size][room]} />
-          )
-        )}
-      </div>
+    <div className="wrapperRoom">
+      <h1 className="room-number">Noom N {roomId}</h1>
+      {Object.keys(rooms).map((size, i) =>
+        Object.keys(rooms[size]).map(
+          (room, i) =>
+            room == roomId && <DrawTable key={i} param={rooms[size][room]} />
+        )
+      )}
+    </div>
   );
 }
 
